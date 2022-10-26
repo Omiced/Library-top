07f1eeb0c7bbd05a55c0d43f15276bf4eb290ffb
Josue Cano <jdevelopcano@gmail.com>
2022-10-25 20:57:07 -0500

    Finish project Add toggle for read or not Add delete function Add events

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const modalBtn = document.querySelector(".btn-modal");
 const modal = document.querySelector(".modal");
 const closeModalBtn = document.querySelector(".close-modal");
 const booksEl = document.querySelector(".books_container");
+const booksContainer = document.querySelector(".books_container");
+//variable de estado
+const myLibrary = [];
+
 //modal event
 modalBtn.addEventListener("click", () => {
   modal.showModal();
@@ -20,11 +24,15 @@ formEl.addEventListener("submit", function (e) {
   e.preventDefault();
   const dataArray = [...new FormData(this)];
   bookData = Object.fromEntries(dataArray);
-  addBookToLibrary(
-    new Book(bookData.title, bookData.author, bookData.pages, bookData.read)
-  );
+  new Book(
+    bookData.title,
+    bookData.author,
+    bookData.pages,
+    bookData.read
+  ).addBookToLibrary();
+
   showBook();
-  console.log(myLibrary);
+  formEl.reset();
   modal.close();
 });
 
@@ -40,11 +48,15 @@ const Book = function (
   this.read = read;
 };
 
-const myLibrary = [];
-const addBookToLibrary = function (book) {
-  myLibrary.push(book);
+Book.prototype.addBookToLibrary = function () {
+  myLibrary.push(this);
 };
 
+Book.prototype.toggleRead = function () {
+  this.read ? (this.read = false) : (this.read = true);
+};
+
+//render book function
 const showBook = function () {
   myLibrary.forEach((book, i) => {
     if (book.render) return;
@@ -53,14 +65,45 @@ const showBook = function () {
       "beforeend",
       `
     <article class="books_card">
-      <h3>Book ${i + 1}</h3>
+      <h3>Book <span class="title-span">${i + 1}</span></h3>
       <p>Title: <span>${book.title}</span></p>
       <p>Author: <span>${book.author}</span></p>
       <p>Pages: <span>${book.pages}</span></p>
-      <p>Read: <span>${book.read ? "Yes" : "No"}</span></p>
-      <button type="button" class="btn-delete">Delete Book</button>
+      <p id="#read">Read: <span>${book.read ? "Yes" : "No"}</span></p>
+      <div class="card-buttons">
+        <button data-item="${i}" type="button" id="btn-toggle">Read or Readn't</button>
+        <button data-item="${i}" type="button"  id="btn-delete">Delete Book</button>
+      </div>
     </article>
   `
     );
   });
+};
+//buttons delete y toggle event and functions handlers
+booksContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.id === "btn-toggle") {
+    const book = e.target.dataset.item;
+    toggle(book);
+  }
+  if (e.target.id === "btn-delete") {
+    const book = e.target.dataset.item;
+    deleteBook(book);
+    console.log(book);
+  }
+});
+const toggle = function (book) {
+  myLibrary[book].toggleRead();
+  updateHTML();
+};
+
+const deleteBook = function (book) {
+  myLibrary.splice(book, 1);
+  updateHTML();
+};
+
+const updateHTML = function () {
+  myLibrary.forEach((book) => delete book.render);
+  booksContainer.innerHTML = "";
+  showBook();
 };
